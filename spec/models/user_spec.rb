cb97bcbeb7242a44bbb206b8374ea4e7e662568a
Irish Fagan <IrishFagan@live.com>
2020-04-23 13:20:23 -0700

    Test user has email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it 'exists' do
-        fake_user = User.new
-    end
+  it 'exists' do
+      fake_user = User.new
+  end
+end
 
+RSpec.describe User, type: :model do
+
+  describe "attributes" do
+    
+    user = User.new(email: "test@testc.com", encrypted_password: 'abc123efg', role: '')
+  
+    it "has email" do
+      expect(user).to respond_to(:email)
+    end
+  
+  end
 end
