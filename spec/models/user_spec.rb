03fb4674b2fbdf9c843dd12d1a1045a11a79477c
Irish Fagan <IrishFagan@live.com>
2020-04-23 13:21:21 -0700

    Remove redundant initialization test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'exists' do
-      fake_user = User.new
-  end
-end
-
-RSpec.describe User, type: :model do
 
   describe "attributes" do
     
