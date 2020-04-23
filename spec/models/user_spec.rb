45d1df46e893be7612ccccd50d9ee14e82a20b51
Irish Fagan <IrishFagan@live.com>
2020-04-23 13:26:06 -0700

    Add let to initialize user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  let(:user) { User.new(email: "test@testc.com", encrypted_password: 'abc123efg', role: '') }
+
   describe "attributes" do
-    
-    user = User.new(email: "test@testc.com", encrypted_password: 'abc123efg', role: '')
   
     it "has email" do
       expect(user).to respond_to(:email)
     end
-  
+
   end
 end
