7272d83a48218366378a3584ad93ce1411bc2b41
Austin Hendricks <ahendricks2@cocc.edu>
2020-04-23 15:19:28 -0700

    Implement simple validation test for region

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -3,21 +3,28 @@ require 'rails_helper'
 RSpec.describe Region, type: :model do
 
 	let(:region) { Region.new }
-	
+
 	# Relationship
 	describe "relationship" do
 		it "has many tickets" do
-			expect(Region.new).to have_many(:tickets)
+			expect(region).to have_many(:tickets)
 		end
 	end
 
 	# Attributes
 	describe "attributes" do
 		it "has a name" do
-			expect(Region.new).to respond_to(:name)
+			expect(region).to respond_to(:name)
 		end
 	end
+
 	# Validations
+	describe "validations" do
+		it "has a name with a min or max value" do
+			expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255)
+		end
+	end
+
 	# Methods
 
 end
