fd20cc127c8a9cb9a18931f4ca316471cfd666eb
Austin Hendricks <ahendricks2@cocc.edu>
2020-04-27 16:53:46 -0700

    Implement relationship tests for resource category

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,8 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
-    it 'exists' do
-        fake_resource_category = ResourceCategory.new
+
+    let (:resource_category) { ResourceCategory.new }
+
+    # Relationship
+    describe "relationship" do
+        it "has and belongs to many organizations" do
+            expect(resource_category).to have_and_belong_to_many(:organizations)
+        end
+
+        it "has many tickets" do
+            expect(resource_category).to have_many(:tickets)
+        end
     end
 
+    # Attributes
+
+    # Validations
+
+    # Methods
 end
