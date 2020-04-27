b9de0b107ea0a5e5363cd50014ab0e6bd927b2b3
Austin Hendricks <ahendricks2@cocc.edu>
2020-04-27 16:58:46 -0700

    Refactor indentation to reflect ruby-esque style

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -6,23 +6,28 @@ RSpec.describe ResourceCategory, type: :model do
 
 	# Relationship
 	describe "relationship" do
-			it "has and belongs to many organizations" do
-					expect(resource_category).to have_and_belong_to_many(:organizations)
-			end
+		it "has and belongs to many organizations" do
+			expect(resource_category).to have_and_belong_to_many(:organizations)
+		end
 
-			it "has many tickets" do
-					expect(resource_category).to have_many(:tickets)
-			end
+		it "has many tickets" do
+			expect(resource_category).to have_many(:tickets)
+		end
 	end
 
 	# Attributes
 	describe "attributes" do
-			it "has a name" do
-					expect(resource_category).to respond_to(:name)
-			end
+		it "has a name" do
+			expect(resource_category).to respond_to(:name)
+		end
 	end
 
 	# Validations
-	
+	describe "validations" do
+		it "has a name with a min or max value" do
+			exp
+		end
+	end
+
 	# Methods
 end
