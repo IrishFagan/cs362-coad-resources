406127b11c7fe95d0fadda838160368f5845362b
Austin Hendricks <ahendricks2@cocc.edu>
2020-04-27 16:57:17 -0700

    Implement attribute test for resource category

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -4,21 +4,18 @@ RSpec.describe Region, type: :model do
 
 	let(:region) { Region.new }
 
-	# Relationship
 	describe "relationship" do
 		it "has many tickets" do
 			expect(region).to have_many(:tickets)
 		end
 	end
 
-	# Attributes
 	describe "attributes" do
 		it "has a name" do
 			expect(region).to respond_to(:name)
 		end
 	end
 
-	# Validations
 	describe "validations" do
 		it "has a name with a min or max value" do
 			expect(region).to validate_length_of(:name).is_at_least(1).is_at_most(255)
@@ -29,7 +26,6 @@ RSpec.describe Region, type: :model do
 		end
 	end
 
-	# Methods
 	describe "methods" do
 		it "creates an Unspecified region if one does not exist" do
 			expect(Region.where(name: 'Unspecified')).to be_empty
--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -2,22 +2,27 @@ require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
 
-    let (:resource_category) { ResourceCategory.new }
+	let (:resource_category) { ResourceCategory.new }
 
-    # Relationship
-    describe "relationship" do
-        it "has and belongs to many organizations" do
-            expect(resource_category).to have_and_belong_to_many(:organizations)
-        end
+	# Relationship
+	describe "relationship" do
+			it "has and belongs to many organizations" do
+					expect(resource_category).to have_and_belong_to_many(:organizations)
+			end
 
-        it "has many tickets" do
-            expect(resource_category).to have_many(:tickets)
-        end
-    end
+			it "has many tickets" do
+					expect(resource_category).to have_many(:tickets)
+			end
+	end
 
-    # Attributes
+	# Attributes
+	describe "attributes" do
+			it "has a name" do
+					expect(resource_category).to respond_to(:name)
+			end
+	end
 
-    # Validations
-
-    # Methods
+	# Validations
+	
+	# Methods
 end
