7bbf20b58b33f49ee2a24ff37bb39c97e4a481bd
Austin Hendricks <ahendricks2@cocc.edu>
2020-04-29 16:08:00 -0700

    Implement belong_to region test

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-    it 'exists' do
-        fake_ticket = Ticket.new
-    end
+
+	let (:ticket) { Ticket.new }
+
+	#relationships
+	describe "relationship" do
+		it "belongs to region" do
+			expect(ticket).to belong_to(:region)
+		end
+		
+	end
+
+	#attributes
+
+	#validations
+
+	#methods
 
 end
