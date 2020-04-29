a8f692d9c066e925b03395801d839734c9cc1340
Austin Hendricks <ahendricks2@cocc.edu>
2020-04-29 16:23:34 -0700

    Implement phone attribute test for ticket

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -25,6 +25,10 @@ RSpec.describe Ticket, type: :model do
 		it "has a name" do
 			expect(ticket).to respond_to(:name)
 		end
+
+		it "has a phone number" do
+			expect(ticket).to respond_to(:phone)
+		end
 	end
 
 	#validations
