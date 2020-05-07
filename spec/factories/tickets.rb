a867b16806435c478549dd0b59da262e04d94337
Irish Fagan <IrishFagan@live.com>
2020-05-07 09:09:24 -0700

    Get factory for ticket setup

---
+++ spec/factories/tickets.rb
@@ -0,0 +1,18 @@
+FactoryBot.define do
+  factory :ticket do
+
+    phone { '+41 44 111 22 33' }
+    region_id { create(:region).id }
+    resource_category_id { create(:resource_category).id }
+
+    trait :open_ticket do
+      name { 'fake_open' }
+      closed { false }
+    end
+    trait :closed_ticket do
+      name { 'fake_closed' }
+      closed { true }
+    end
+
+  end
+end
