1eaef4fa7c820bd1769fe4416cbf460559bd57f1
Irish Fagan <IrishFagan@live.com>
2020-05-21 14:17:16 -0700

    Implement partial test for seeing if an admin can create a region

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+  it "creates a new region as an admin" do
+    admin_user = FactoryBot.create(:user, :admin)
+  
+    log_in_as(admin_user)
+    visit(new_region_path)
+  
+    fill_in 'Name', with: 'Fake Region'
+  end
+
 end
