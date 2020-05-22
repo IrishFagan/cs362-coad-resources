a144f3245249a7085d885c8d633e051539b760de
Austin Hendricks <hendraus@oregonstate.edu>
2020-05-22 15:14:43 -0700

    Add user confirmation to User factory and implement region deletion test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,11 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "test@test#{n}.com" }
     password { "password" }
+
+    after :create do |user|
+    	user.confirm
+    end
+    
   end
 
   trait :admin do
