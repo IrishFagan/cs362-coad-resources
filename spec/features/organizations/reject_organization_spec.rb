b762529c9a6fd2b39d55bcfe3bd2928a3ef4a9de
Austin Hendricks <hendraus@oregonstate.edu>
2020-05-26 18:15:56 -0700

    Implement organization rejection feature test

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
 
+	let(:admin) { (User.new(email: 'fakeadmin@fake.com', password: 'password', role: :admin)) }
+
+	it "rejects an organization that a regualr user has submitted" do
+		
+		organization = FactoryBot.create(:organization, :submitted)    
+
+		admin.confirm
+		visit(login_path)
+		log_in_as(admin)
+
+    visit(organizations_path)
+    click_on('Pending')
+    click_on('Review')
+
+    click_on('Reject')
+
+    expect(page).to have_content('has been rejected')
+  end
+
 end
