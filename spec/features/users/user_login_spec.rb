aab62ac96c209d9d85875ace4c8fc4a34b7ac19d
Austin Hendricks <hendraus@oregonstate.edu>
2020-05-26 18:20:02 -0700

    Fix minor indentation errors

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+	
 	let(:admin) { (User.new(email: 'unique@test.com', password: 'password', role: :admin)) }
 
 	it "successfully logs in as an admin" do
@@ -11,6 +12,7 @@ RSpec.describe 'Logging in', type: :feature do
 		fill_in 'Password', with: 'password'
 		click_on 'Sign in'
 		expect(page).to have_content('Signed in successfully')
+
 	end
 	
 end
