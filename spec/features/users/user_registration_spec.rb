67698ec7bd2b0e3777b0f627beb6179ac57e7aaf
Austin Hendricks <hendraus@oregonstate.edu>
2020-05-26 18:12:05 -0700

    Implement User registration happy path test

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -2,20 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
 
-## Feel free to uncomment and work on this one, unsure of how to simulate pressing the recaptcha button...
+# Feel free to uncomment and work on this one, unsure of how to simulate pressing the recaptcha button...
 
-#	it "signs up successfully as organization user" do
-#		visit(signup_path)
-#		LoginController.any_instance.expects(:verify_recaptcha).returns(true)
-#		
-#		fill_in 'Email address', with: 'signup@test.com'
-#		fill_in 'Password', with: 'password'
-#		fill_in 'Password confirmation', with: 'password'
-#		click_on 'I\'m not a robot'
-#		click_on 'Sign up'
-#
-#		expect(page).to have_content('successfully')
-#
-#	end
+	it "signs up successfully as organization user" do
+		visit(signup_path)
+		
+		fill_in 'Email address', with: 'signup@test.com'
+		fill_in 'Password', with: 'password'
+		fill_in 'Password confirmation', with: 'password'
+		click_on 'commit'
+		expect(page).to have_content('confirmation link')
 
+	end
 end
