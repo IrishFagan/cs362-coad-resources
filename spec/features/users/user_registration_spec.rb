ce6a2fd66216d2f1e09b6894739727639b3438f9
Austin Hendricks <hendraus@oregonstate.edu>
2020-05-29 12:50:24 -0700

    Finish user registration feature test

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe 'User registration', type: :feature do
 		fill_in 'Password confirmation', with: 'password'
 		click_on 'commit'
 		expect(page).to have_content('confirmation link')
+		user = User.find_by(email: "signup@test.com")
+		visit(user_confirmation_path(confirmation_token: user.confirmation_token))
 
 	end
 end
