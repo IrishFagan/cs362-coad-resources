26dd2118e6e3d17d5de8bff29f242b00cf9c2c0f
Irish Fagan <IrishFagan@live.com>
2020-06-01 12:53:22 -0700

    Add test where an admin closes a ticket

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
 
+  it "deletes a ticket" do
+    admin = FactoryBot.create(:user, :admin)
+    ticket = FactoryBot.create(:ticket, :open_ticket)
+  
+    log_in_as(admin)
+  
+    visit(dashboard_path)
+    click_on('fake_open')
+    click_on('Close')
+    click_on('fake_open')
+
+    expect(page).to have_content('Status Closed')
+
+  end
+
 end
