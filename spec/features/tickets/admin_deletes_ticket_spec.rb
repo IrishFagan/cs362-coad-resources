56e53845b35cb3cc92cab8d945ae73f87b29f22e
Irish Fagan <IrishFagan@live.com>
2020-06-01 12:44:03 -0700

    Add test where an admin deletes an open ticket

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
 
+  it "deletes a ticket" do
+    admin = FactoryBot.create(:user, :admin)
+    ticket = FactoryBot.create(:ticket, :open_ticket)
+  
+    log_in_as(admin)
+  
+    visit(dashboard_path)
+    click_on('fake_open')
+    click_on('Delete')
+
+    expect(page).to have_content('was deleted')
+
+  end
+
 end
