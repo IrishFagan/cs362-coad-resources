5b43fe49f9401ad854b49b5355900c283723e003
Austin Hendricks <hendraus@oregonstate.edu>
2020-06-01 20:00:07 -0700

    Joking, still refactoring ticket creation

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe 'Creating a Ticket', type: :feature do
 
   it "creates a new ticket" do
 
-    region = FactoryBot.create(:region)
-    resource_category = FactoryBot.create(:resource_category, :active)
+    #region = FactoryBot.create(:region)
+    #resource_category = FactoryBot.create(:resource_category, :active)
     visit(new_ticket_path)
 
     fill_in('Full Name', with: 'Fake Name')
     fill_in('Phone Number', with: '503-910-4740')
-    select('Fake Region 3', from: "ticket_region_id")
-    select('Resource Category Name 3', from: "ticket_resource_category_id")
+    select('Select', from: "ticket_region_id")
+    select('Select', from: "ticket_resource_category_id")
     fill_in('Description', with: 'Fake Descripition')
 
     click_on('Send this help request')
